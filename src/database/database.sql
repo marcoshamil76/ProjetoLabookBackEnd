b5c55ab9dbe021ad773dfe1c7682303bc1378403
marcoshamil76 <marcoshamilton1976@gmail.com>
2023-02-15 22:55:13 -0300

    checkDB-ok

--- src/database/database.sql
+++ src/database/database.sql
@@ -12,8 +12,8 @@ CREATE TABLE posts(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     creator_id TEXT NOT NULL,
     content TEXT NOT NULL,
-    likes INTEGER,
-    dislikes INTEGER,
+    likes INTEGER DEFAULT (0) NOT NULL,
+    dislikes INTEGER DEFAULT (0) NOT NULL,
     created_at TEXT DEFAULT (DATETIME()),
     updated_at TEXT DEFAULT (DATETIME()),
     FOREIGN KEY (creator_id) REFERENCES users (id)
@@ -23,8 +23,12 @@ CREATE TABLE likes_dislikes(
     user_id TEXT NOT NULL,
     post_id TEXT NOT NULL,
     like INTEGER,
-    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
     FOREIGN KEY (post_id) REFERENCES posts (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
 );
 
 SELECT * FROM users;
